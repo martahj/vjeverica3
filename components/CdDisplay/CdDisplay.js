95461d883177907627f9bb4f77ab88e461e08026
martahj <marta.hourigan.johnson@gmail.com>
2017-07-09 17:42:58 -0500

    styling on contact page

--- components/CdDisplay/CdDisplay.js
+++ components/CdDisplay/CdDisplay.js
@@ -6,42 +6,71 @@ type CdDisplayProps = {
   cd: {
     image: string,
     title: string,
+    tracks: Array<{
+      main: string,
+      finePrint?: string
+    }>,
+    artists: Array<string>,
+    payment: string
   }
 };
 
 const displayWrapperStyle = {
-  // flexWrap: 'nowrap',
   display: 'flex',
+  flexWrap: 'wrap',
 };
 
-const displayBoxStyle = {
+const displayBox1Style = {
   display: 'flex',
   flexDirection: 'column',
+  maxWidth: '300px',
+  flexWrap: 'wrap',
+};
+
+const displayBox2Style = {
+  display: 'flex',
+  flexDirection: 'column',
+  minWidth: '300px',
+  flexWrap: 'wrap',
+  flexBasis: '50%',
+};
+
+const finePrintTextStyle = {
+  fontSize: 12,
+};
+
+const artistListStyle = {
+  listStyleType: 'none',
+  margin: 0,
+  fontSize: 14,
 };
 
 const CdDisplay = ({ cd }: CdDisplayProps) => (
   <div style={displayWrapperStyle}>
-    <div style={displayBoxStyle}>
+    <div style={displayBox1Style}>
       <h3>{cd.title}</h3>
       <Image publicId={cd.image} width="300" crop="scale" />
       <p>
         {cd.payment.cost}
       </p>
     </div>
-    <div style={displayBoxStyle}>
-      <ol>
-        {cd.tracks.map(track => (
-          <li key={track.main}>
-            <p>{track.main}</p>
-            <p>{track.finePrint}</p>
-          </li>
-        ))}
-      </ol>
-      <ul>
-        {cd.artists.map(artist => (
-          <li key={artist}>{artist}</li>
-        ))}
-      </ul>
+    <div style={displayBox2Style}>
+      <div style={{ minWidth: 300 }}>
+        <ol>
+          {cd.tracks.map(track => (
+            <li key={track.main}>
+              {track.main}
+              <br />
+              <span style={finePrintTextStyle}>{track.finePrint}</span>
+            </li>
+          ))}
+        </ol>
+        <ul style={artistListStyle}>
+          {cd.artists.map(artist => (
+            <li key={artist}>{artist}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   </div>
 );
